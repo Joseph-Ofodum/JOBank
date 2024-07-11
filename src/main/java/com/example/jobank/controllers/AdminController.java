661c0c76b13a039537e8aa8bf8706b7dc79f3988
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-07-11 09:26:31 +0100

    Admin controller

---
+++ src/main/java/com/example/jobank/controllers/AdminController.java
@@ -0,0 +1,11 @@
+package com.example.jobank.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("")
+public class AdminController {
+}
--- src/main/java/com/example/jobank/service/serviceImpl/CustomerServiceImpl.java
+++ src/main/java/com/example/jobank/service/serviceImpl/CustomerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.jobank.service.serviceImpl;
 
+import com.example.jobank.repositories.CustomerRepository;
 import com.example.jobank.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
 }
